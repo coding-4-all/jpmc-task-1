50440622bdfd523b838f506ffaf5cb5833e7f9c8
mesfint <mesfin_meng@yahoo.com>
2023-01-18 22:49:05 +0200

    task1-solution completed

--- task1Env/bin/activate.fish
+++ task1Env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mesfin/Documents/JPMorgan/jpmc-task-1/task1Env"
+set -gx VIRTUAL_ENV "/home/mesfin/Documents/JPMorgan/jpmc-task-1.1/task1Env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
